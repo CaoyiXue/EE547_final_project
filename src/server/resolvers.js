251c3fdeaad02911e635c1e3fe38ebfd7dc1b4eb
caoyi xue <caoyixue@usc.edu>
2023-05-08 04:57:53 -0700

    final

--- src/server/resolvers.js
+++ src/server/resolvers.js
@@ -1,10 +1,3 @@
-const resolvers = {
-  // Business: {
-  //   waitTime: (obj, args, context, info) => {
-  //     let options = [0, 5, 10, 15, 30, 45];
-  //     return options[Math.floor(Math.random() * options.length)];
-  //   },
-  // },
-};
+const resolvers = {};
 
 export default resolvers;
